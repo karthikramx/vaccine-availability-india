e992204588078d35f66fe56a5c67f39259e6bd23
Karthik Ram <karthikram570@gmail.com>
2021-05-09 13:10:15 +0530

    Exports vaccine availability to csv file

--- main.py
+++ main.py
@@ -32,51 +32,64 @@ def check_availability(pin_code):
     button = driver.find_element_by_class_name("pin-search-btn")
     button.click()
     time.sleep(2)
-    page_source = driver.page_source
-    soup = BeautifulSoup(page_source, 'html.parser')
+    driver.execute_script("window.scrollTo(0, 700)")
 
-    date_result = soup.find_all('li', attrs={'class': 'availability-date'})
-    hostipal_names_result = soup.find_all('h5', attrs={'class': 'center-name-title'})
-    hostipal_address_result = soup.find_all('p', attrs={'class': 'center-name-text'})
-    row_result = soup.find_all('ul', attrs={'class': 'slot-available-wrap'})
+    master_vaccine_dataframe = pd.DataFrame()
+    weeks = 5
+    for i in range(weeks):
+        page_source = driver.page_source
+        soup = BeautifulSoup(page_source, 'html.parser')
+        date_result = soup.find_all('li', attrs={'class': 'availability-date'})
 
+        hostipal_names_result = soup.find_all('h5', attrs={'class': 'center-name-title'})
+        hostipal_address_result = soup.find_all('p', attrs={'class': 'center-name-text'})
+        row_result = soup.find_all('ul', attrs={'class': 'slot-available-wrap'})
 
-    hostipal_names = []
-    hostipal_addresses = []
-    current_dates = []
+        hostipal_names = []
+        hostipal_addresses = []
+        current_dates = []
 
-    for date in date_result:
-        current_dates.append(date.text)
+        for date in date_result:
+            current_dates.append(date.text)
 
-    for hospital in hostipal_names_result:
-        hostipal_names.append(hospital.text)
+        for hospital in hostipal_names_result:
+            hostipal_names.append(hospital.text)
 
-    for hospital_address in hostipal_address_result:
-        hostipal_addresses.append(hospital_address.text)
+        for hospital_address in hostipal_address_result:
+            hostipal_addresses.append(hospital_address.text)
 
-    all_tag_list = []
+        all_tag_list = []
 
-    for row in row_result:
-        als = row.find_all('li')
-        row_wise_list = []
-        for al in als:
-            row_wise_list.append(al.div.text)
-        all_tag_list.append(row_wise_list)
+        for row in row_result:
+            als = row.find_all('li')
+            row_wise_list = []
+            for al in als:
+                row_wise_list.append(al.div.text)
+            all_tag_list.append(row_wise_list)
 
-    print(hostipal_names, "\n", "SIZE:{}".format(len(hostipal_names)))
-    print(hostipal_addresses, "\n", "SIZE:{}".format(len(hostipal_addresses)))
-    print(all_tag_list, "\n", "SIZE:{}".format(len(all_tag_list)))
+        print("\n**********************************************************************************\n")
 
-    columns_list = current_dates
-    vaccine_data = pd.DataFrame(index=hostipal_names, columns=columns_list[0:6])
+        print(hostipal_names, "\n", "SIZE:{}".format(len(hostipal_names)))
+        print(hostipal_addresses, "\n", "SIZE:{}".format(len(hostipal_addresses)))
+        print(all_tag_list, "\n", "SIZE:{}".format(len(all_tag_list)))
 
-    for x in range(len(hostipal_names)):
-        vaccine_data.loc[hostipal_names[x]] = all_tag_list[x]
+        columns_list = current_dates[i * 6: (i * 6) + 6]
+        vaccine_data = pd.DataFrame(index=hostipal_names, columns=columns_list)
 
-    print(vaccine_data.head())
+        for x in range(len(hostipal_names)):
+            vaccine_data.loc[hostipal_names[x]] = all_tag_list[x]
+
+        master_vaccine_dataframe = pd.concat([master_vaccine_dataframe, vaccine_data], axis=1)
+
+        print(vaccine_data)
+        print(master_vaccine_dataframe)
+
+        next_button = driver.find_element_by_class_name("right.carousel-control.carousel-control-next")
+        next_button.click()
+        time.sleep(2)
 
     print("check")
-
+    master_vaccine_dataframe.to_csv('Vaccine_Data_{}week_pin{}.csv'.format(weeks, pin_code))
     driver.quit()
 
 
